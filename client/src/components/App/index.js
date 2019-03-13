75814a49937a8739a3b1876c43670fc983d62b47
oleh <gladkiioleg1999@gmail.com>
2019-03-13 13:35:34 +0200

    change server

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -53,13 +53,13 @@ const App = () => {
       }
 
       const messageReceive = (msg) => {
+            axios.post('https://calm-citadel-92548.herokuapp.com/api/messages', msg)
+            .then(res => console.log(res.data))
+            .catch(err => console.log(err))
             setMessages((prevMsg) => [...prevMsg, msg] );
       }
 
       const messageSend = (msg) => {
-            axios.post('https://calm-citadel-92548.herokuapp.com/api/messages', msg)
-            .then(res => console.log(res.data))
-            .catch(err => console.log(err))
             msg.user = userName.value;
             socket.emit('send:message', msg);
       }
